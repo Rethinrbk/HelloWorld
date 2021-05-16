7756a85f6cedac601452cbf2479eae9601bc334c
unknown <Rethin.rbk@gmail.com>
2021-05-16 18:52:24 +0800

    first commit

---
+++ Practise/src/main/java/com/helloWorld/test/Practise/App.java
@@ -0,0 +1,13 @@
+package com.helloWorld.test.Practise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
